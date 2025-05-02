c3b1871417d6053e96605f02978db79c024a82a3
Vishal Bhandari <vishal_bhandari@hotmail.com>
2025-05-02 16:12:00 +0100

    committed

--- DemoBlazer_app/src/test/java/StepDefinition/Add_to_cart_steps.java
+++ DemoBlazer_app/src/test/java/StepDefinition/Add_to_cart_steps.java
@@ -19,17 +19,15 @@ public class Add_to_cart_steps {
 	private int priceTwo;
 	private int priceThree;
 	private int result;
-	
+
 	private String ActSamsungTitle;
 	private String ActNexusTitle;
 	private String ActIPhone;
-
 
 	AddToCartActions addtocartActions = new AddToCartActions();
 
 	@And("I add samsung galaxy s6, Iphone6, Nexus6 to cart")
 	public void i_add_samsung_galaxy_s6_iphone6_nexus6_to_cart() throws InterruptedException {
-
 
 		addtocartActions.clickSamsungGalaxyS6();
 		addtocartActions.ClickAddToCartButton();
@@ -51,7 +49,6 @@ public class Add_to_cart_steps {
 		Thread.sleep(4000);
 		alt.accept();
 
-
 	}
 
 	@And("I click cart")
@@ -60,55 +57,45 @@ public class Add_to_cart_steps {
 		addtocartActions.ClickCartLink();
 		Thread.sleep(5000);
 
-
 	}
-
 
 	@Then("I should see the following on the products page:")
 	public void i_should_see_the_following_on_the_products_page(DataTable dataTable) {
 
-		List<Map<String, String>>userList = dataTable.asMaps(String.class, String.class);
+		List<Map<String, String>> userList = dataTable.asMaps(String.class, String.class);
 
-
-		//System.out.println("This is my map" +userList);
-		//System.out.println("This is my samsung title" +userList.get(2).get("Title"));
+		// System.out.println("This is my map" +userList);
+		// System.out.println("This is my samsung title" +userList.get(2).get("Title"));
 
 		this.ActSamsungTitle = addtocartActions.SamsungGalaxyGetTitle();
 		this.ActNexusTitle = addtocartActions.Nexus6GetTitle();
 		this.ActIPhone = addtocartActions.IPhone6GetTitle();
 
-
 		assertEquals(userList.get(0).get("Title"), this.ActSamsungTitle);
 		assertEquals(userList.get(1).get("Title"), this.ActIPhone);
 		assertEquals(userList.get(2).get("Title"), this.ActNexusTitle);
-
-
 
 	}
 
 	@And("I sum up the items in my cart")
 	public void i_sum_up_the_items_in_my_cart(DataTable dataTable) {
 
-		List<Map<String, Integer>>priceList = dataTable.asMaps(String.class, Integer.class);
+		List<Map<String, Integer>> priceList = dataTable.asMaps(String.class, Integer.class);
 
-		
 		this.priceOne = priceList.get(0).get("Price");
 		this.priceTwo = priceList.get(1).get("Price");
 		this.priceThree = priceList.get(2).get("Price");
 
 		this.result = priceOne + priceTwo + priceThree;
 
-	//	System.out.println("This is my result -> " + result);
+		System.out.println("This is my result -> " + result);
 
 	}
-
 
 	@Then("the total value of the items should be {int}")
 	public void the_total_value_of_the_items_should_be(Integer int1) {
-		
+
 		assertEquals(this.result, addtocartActions.TotalPrice());
 	}
-
-
 
 }
